e8112d8af27ed2c136acbcfff4b06105efdf1977
Yerdaulet <yerdaulet.zhumabay@gmail.com>
2026-04-17 10:05:52 +0500

    add data

---
+++ eks/dev/data.tf
@@ -0,0 +1,26 @@
+data "aws_caller_identity" "current" {}
+
+data "terraform_remote_state" "vpc" {
+  backend = "s3"
+  config = {
+    bucket  = "epam-terrform-state-bucket"
+    key     = "895587011312_NetworkAdministrator/vpc/dual-stack/eks/dev/terraform.tfstate"
+    region  = "eu-central-1"
+    profile = "895587011312_AdministratorAccess"
+  }
+}
+
+# Fetch the NVIDIA-optimized AMI version
+data "aws_ssm_parameter" "eks_ami_nvidia_amazon_linux_2023" {
+  name = "/aws/service/eks/optimized-ami/${var.cluster_version}/amazon-linux-2023/x86_64/nvidia/recommended/release_version"
+}
+
+# General-purpose EKS-optimized AMI
+data "aws_ssm_parameter" "eks_ami_amazon_linux_2023" {
+  name = "/aws/service/eks/optimized-ami/${var.cluster_version}/amazon-linux-2023/arm64/standard/recommended/release_version"
+}
+
+data "aws_route53_zone" "zones" {
+  for_each = toset(local.domains)
+  name     = each.value
+}
