fa4156f9e26bff7f87279aad4547164d7e7444cd
Yerdaulet <yerdaulet.zhumabay@gmail.com>
2026-04-18 17:15:50 +0500

    add 'dynamic' aws_eks_access_entry

---
+++ eks/dev/aws_eks_access_entry.tf
@@ -0,0 +1,17 @@
+resource "aws_eks_access_entry" "this" {
+  # distinct(list) - Removes duplicates, keeps original order
+  # toset(list)    - Removes duplicates, but converts to a Set (loses order, often sorts)
+  # flatten(list)  - Collapses nested lists into a single flat list (often used before distinct).
+
+  # Some users may require multiple policies to be attched at aws_eks_access_policy_association resource
+  for_each = toset([for entry in var.cluster_access_config : entry.user_arn])
+
+  cluster_name  = aws_eks_cluster.main.name
+  principal_arn = each.value
+  # Extract unique groups for this specific user from the config list
+  kubernetes_groups = distinct(flatten([
+    for entry in var.cluster_access_config : entry.kubernetes_groups
+    if entry.user_arn == each.value
+  ]))
+  type = "STANDARD"
+}
