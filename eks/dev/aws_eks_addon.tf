f1dec10dd428ff72ca31a639f0b51e17717e3416
Yerdaulet <yerdaulet.zhumabay@gmail.com>
2026-04-19 10:46:56 +0500

    add vpc cdn addon

--- eks/dev/aws_eks_addon.tf
+++ eks/dev/aws_eks_addon.tf
@@ -71,3 +71,53 @@ resource "aws_eks_addon" "kube_proxy" {
   resolve_conflicts_on_update = "PRESERVE"
   preserve                    = true
 }
+
+# ------ VPC CNI Addon ------
+data "aws_iam_policy_document" "vpc_cni_pod_identity" {
+  statement {
+    actions = ["sts:AssumeRole", "sts:TagSession"]
+    effect  = "Allow"
+
+    principals {
+      type        = "Service"
+      identifiers = ["pods.eks.amazonaws.com"]
+    }
+  }
+}
+
+resource "aws_iam_role" "cni_role" {
+  name               = "${aws_eks_cluster.main.name}-vpc-cni-role"
+  assume_role_policy = data.aws_iam_policy_document.vpc_cni_pod_identity.json
+}
+
+resource "aws_eks_pod_identity_association" "vpc_cni" {
+  cluster_name    = aws_eks_cluster.main.name
+  namespace       = "kube-system"
+  service_account = "aws-node"
+  role_arn        = aws_iam_role.cni_role.arn
+}
+
+resource "aws_eks_addon" "vpc_cni" {
+  cluster_name  = aws_eks_cluster.main.name
+  addon_name    = "vpc-cni"
+  addon_version = data.aws_eks_addon_version.latest_vpc_cni.version
+
+  resolve_conflicts_on_create = "OVERWRITE"
+  resolve_conflicts_on_update = "PRESERVE"
+  preserve                    = true
+
+  configuration_values = jsonencode({
+    env = {
+      # Enable Prefix Delegation for high pod density.
+      # See: https://docs.aws.amazon.com/eks/latest/userguide/cni-increase-ip-addresses.html
+      ENABLE_PREFIX_DELEGATION = "true"
+      MINIMUM_IP_TARGET        = "16"
+      WARM_IP_TARGET           = "10"
+      WARM_PREFIX_TARGET       = "0"
+
+      # Recommended to avoid "stuck traffic" (timeouts) & NetworkPolicies wont take affect when the PODs in the same node.
+      # All because of the shorcut of packets, if it sees the destination being local then directly delivers to POD by skipping Policies.
+      DISABLE_TCP_EARLY_DEMUX = "true"
+    }
+  })
+}
