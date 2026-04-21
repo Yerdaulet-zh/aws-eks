150e872e5880e45148aebba1d4b2aa653678b3ca
Yerdaulet <yerdaulet.zhumabay@gmail.com>
2026-04-21 14:47:26 +0500

    add module for creation of dev eks clsuter based on the module eks

---
+++ eks/dev/data.tf
@@ -0,0 +1,50 @@
+data "aws_caller_identity" "current" {}
+
+data "terraform_remote_state" "vpc" {
+  backend = "s3"
+  config = {
+    bucket  = "epam-terrform-state-bucket"
+    key     = "895587011312_NetworkAdministrator/vpc/dual-stack/eks/dev/terraform.tfstate"
+    region  = "eu-central-1"
+    profile = "895587011312_AdministratorAccess"
+  }
+}
+
+# Fetch the NVIDIA-optimized AMI version
+data "aws_ssm_parameter" "eks_ami_nvidia_amazon_linux_2023" {
+  name = "/aws/service/eks/optimized-ami/${var.cluster_config.cluster_version}/amazon-linux-2023/x86_64/nvidia/recommended/release_version"
+}
+
+# General-purpose EKS-optimized AMI
+data "aws_ssm_parameter" "eks_ami_amazon_linux_2023" {
+  name = "/aws/service/eks/optimized-ami/${var.cluster_config.cluster_version}/amazon-linux-2023/arm64/standard/recommended/release_version"
+}
+
+data "aws_vpc" "this" {
+  id = data.terraform_remote_state.vpc.vpc_id
+}
+
+# Addons
+data "aws_eks_addon_version" "latest_coredns" {
+  addon_name         = "coredns"
+  kubernetes_version = aws_eks_cluster.main.version
+  most_recent        = true
+}
+
+data "aws_eks_addon_version" "latest_kube_proxy" {
+  addon_name         = "kube-proxy"
+  kubernetes_version = aws_eks_cluster.main.version
+  most_recent        = true
+}
+
+data "aws_eks_addon_version" "latest_vpc_cni" {
+  addon_name         = "vpc-cni"
+  kubernetes_version = aws_eks_cluster.main.version
+  most_recent        = true
+}
+
+data "aws_eks_addon_version" "latest_ebs_csi" {
+  addon_name         = "aws-ebs-csi-driver"
+  kubernetes_version = aws_eks_cluster.main.version
+  most_recent        = true
+}
---
+++ eks/dev/main.tf
@@ -0,0 +1,67 @@
+module "eks_dev" {
+  source = "../../modules/eks"
+  cluster_access_config = [{
+    user_arn          = data.aws_caller_identity.current.arn
+    kubernetes_groups = []
+    policy_arn        = "arn:aws:eks::aws:cluster-access-policy/AmazonEKSClusterAdminPolicy"
+    access_scope = {
+      type       = "cluster"
+      namespaces = []
+    }
+  }]
+
+  kubernetes_network_config = {
+    ip_family = "ipv6"
+    service_ipv4_cidr = {
+      use_custom_service_cidr  = true
+      vpc_cidr_prefix_to_check = "10."
+      first_cidr               = "172.20.0.0/16" # If VPC is 10.x, use 172.x
+      second_cidr              = "10.100.0.0/16" # If VPC is not 10.x, use 10.x
+    }
+  }
+
+  node_group_configs = {
+    # General purpose nodes
+    "app1" = {
+      node_group_name = "app-workloads-1"
+      instance_types  = ["t3.small", "t3.medium", "t3.large"]
+      capacity_type   = "ON_DEMAND"
+      subnet_ids      = [data.terraform_remote_state.vpc.outputs.public_dual_stack_subnets[0]]
+      role_key        = "app*"
+      desired_size    = 1
+      max_size        = 2
+      min_size        = 0
+      labels          = { role = "state-full-less-apps" }
+      taints          = []
+    },
+    "app2" = {
+      node_group_name = "app-workloads-2"
+      instance_types  = ["t3.small", "t3.medium", "t3.large"]
+      capacity_type   = "ON_DEMAND"
+      subnet_ids      = [data.terraform_remote_state.vpc.outputs.public_dual_stack_subnets[0]]
+      role_key        = "app*"
+      desired_size    = 1
+      max_size        = 2
+      min_size        = 0
+      labels          = { role = "state-full-less-apps" }
+      taints          = []
+    },
+    # Spot instances
+    "ai-ml-workers" = {
+      node_group_name = "ai-ml-workers"
+      instance_types  = ["t4g.small", "t4g.medium", "t4g.large"]
+      capacity_type   = "SPOT"
+      subnet_ids      = [data.terraform_remote_state.vpc.outputs.public_dual_stack_subnets[0]]
+      role_key        = "ai-ml-workloads"
+      desired_size    = 1
+      max_size        = 2
+      min_size        = 0
+      labels          = { role = "ai-worker" }
+      taints = [{
+        key    = "workload"
+        value  = "heavy"
+        effect = "NO_SCHEDULE"
+      }]
+    }
+  }
+}
