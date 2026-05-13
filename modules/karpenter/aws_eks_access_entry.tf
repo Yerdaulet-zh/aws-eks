816a11e1d4605c931181f31a1231bf0c2accd40b
Yerdaulet <yerdaulet.zhumabay@gmail.com>
2026-05-13 19:05:27 +0500

    add aws_eks_access_entry for karpenter node role

---
+++ modules/karpenter/aws_eks_access_entry.tf
@@ -0,0 +1,8 @@
+resource "aws_eks_access_entry" "this" {
+  cluster_name  = var.cluster_name
+  principal_arn = aws_iam_role.karpenter_node_role.arn
+
+  kubernetes_groups = []
+
+  type = "EC2_LINUX"
+}
